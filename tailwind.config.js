b77ebaa35efc3cbfae4c9656bd3cbdeffcb19033
Luciano Bueno <glamero@gmail.com>
2021-05-01 23:36:25 -0300

    Fixed purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // purge: [],
-	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	purge: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   // theme: {
   //   extend: {},
